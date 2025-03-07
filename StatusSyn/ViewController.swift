617657855a443b63e5ed489c61597b7fb01c6874
Yao-lin101 <1017789696@qq.com>
2025-03-07 11:32:24 +0800

    Initial Commit

---
+++ StatusSyn/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StatusSyn
+//
+//  Created by Enkidu ㅤ on 2025/3/7.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
